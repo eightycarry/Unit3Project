b8641d92d10ee13a20739cc391d47393d9f787a6
eightycarry <calvin_lor@yahoo.com>
2020-12-07 15:40:10 -0600

    Unit3Project

--- pf.js
+++ pf.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
+    $("#tabs a").click(showTab);
+
+    function showTab(event) {
+        event.preventDefault();
+        $(this).tab("show");
+        $("form").submit();
+    }
 
 });
