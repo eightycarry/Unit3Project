e0917b8a2d4f84801ec8f0e8f398b2312502ab26
eightycarry <calvin_lor@yahoo.com>
2020-12-07 16:59:43 -0600

    Unit3Project

--- pf.js
+++ pf.js
@@ -4,7 +4,28 @@ $(document).ready(function () {
     function showTab(event) {
         event.preventDefault();
         $(this).tab("show");
-        $("form").submit();
+        $("form").submit(financeCalc);
+    }
+
+    function financeCalc(event) {
+        event.preventDefault();
+
+        const userData = {
+            name: $("#name").val(),
+            income: parseFloat($("#income").val()),
+            bills: parseFloat($("#bills").val()),
+            food: parseFloat($("#food").val()),
+            housing: parseFloat($("#housing").val()),
+            transportation: parseFloat($("#transportation").val()),
+        }
+
+        var necessities = parseFloat(userData.income * 0.50).toFixed(2);
+        var wants = parseFloat(userData.income * 0.30).toFixed(2);
+        var save = parseFloat(userData.income * 0.20).toFixed(2);
+
+        $("#necessities").text(necessities);
+        $("#wants").text(wants);
+        $("#save").text(save);
     }
 
 });
