8d0b2b6f6889d83d1e412483f1a9f01c71262c5e
eightycarry <calvin_lor@yahoo.com>
2020-12-08 15:39:01 -0600

    Unit3Project

--- pf.js
+++ pf.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     function showTab(event) {
         event.preventDefault();
         $(this).tab("show");
-        $("form").submit(financeCalc);
+        $("#financeButton").click(financeCalc);
     }
 
     function financeCalc(event) {
@@ -13,10 +13,26 @@ $(document).ready(function () {
         const userData = {
             name: $("#name").val(),
             income: parseFloat($("#income").val()),
+        }
+
+        const userNeeds = {
             bills: parseFloat($("#bills").val()),
             food: parseFloat($("#food").val()),
             housing: parseFloat($("#housing").val()),
             transportation: parseFloat($("#transportation").val()),
+            utilities: parseFloat($("#utilities").val()),
+        }
+
+        const userWants = {
+            subs: parseFloat($("#subs").val()),
+            travel: parseFloat($("#travel").val()),
+            entertainment: parseFloat($("#entertainment").val()),
+            mealsOut: parseFloat($("#mealsOut").val()),
+        }
+
+        const userSaves = {
+            savings: parseFloat($("#savings").val()),
+            debt: parseFloat($("#debt").val()),
         }
 
         var necessities = parseFloat(userData.income * 0.50).toFixed(2);
